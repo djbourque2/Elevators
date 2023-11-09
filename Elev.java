765c64f451099f40c10ab8cc351c9a2d24077a5b
djbourque2 <98372250+djbourque2@users.noreply.github.com>
2023-11-09 15:13:29 -0800

    starting out

---
+++ Elev.java
@@ -0,0 +1,23 @@
+import java.util.Properties;
+
+public class Elev {
+
+    public static void main(String[]args){
+    //read from args[0] and configure the following elements:
+        /*
+         * structures          default = linked
+         * floors              default = 32
+         * passengers          default = 0.03
+         * elevators           default = 1
+         * elevatorCapacity    default = 10
+         * duration            default = 500
+         */
+
+        Properites structures = new Properties("linked");
+
+        if (args[0] != null){
+
+        }
+
+    }
+}
