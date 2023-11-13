e1303b307a5570ce05090714b858b849258cd8d4
djbourque2 <djbourque2@dons.usfca.edu>
2023-11-12 17:11:42 -0800

    Update Elev.java

--- .idea/src/Elev.java
+++ .idea/src/Elev.java
@@ -1,9 +1,7 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.AbstractList;
-import java.util.LinkedList;
-import java.util.Properties;
+import java.util.*;
 
 class Elevators {
     //todo: set semi-configurable states/values
@@ -11,8 +9,32 @@ class Elevators {
      * curr_floor,  current floor position
      * num_pass,    number of passengers
      * max_pass,    maximum number of passengers
-     * pass_stack,  
      */
+    // pass-floor_stack,  determines the order in which the passengers are dropped off first
+}
+
+class Floors {
+    //use case: in an array/linked list, where an elevator iterates forward/backward through the array to simulate moving
+    /*
+     * pass_pending,    queue for waiting passengers
+     */
+    /* functions:
+        gen_pass()  takes the passengers chance + others as parameters to add a new passenger (represented as an int)
+                    to the pass_pending stack
+     */
+
+    Queue pass_pending;
+
+    public Floors Floors(){
+        ArrayList pass = new ArrayList();
+    }
+
+    public void gen_pass(int floors, int passengers){
+        Random rand = new Random();
+        if (rand.nextDouble() <= passengers){
+            pass_pending.add(rand.nextInt(floors + 1) + 0);
+        }
+    }
 }
 
 public class Elev {
@@ -33,6 +55,10 @@ public class Elev {
         int elev_num = 1; //elevators
         int elev_Cap = 10; //elevatorCapacity
         int dur = 500; //duration
+
+        Floors floors1;
+
+        floors1.pass_pending.add(1);
 
         if (args[0] != null){
             try (FileReader reader = new FileReader(args[0])){
